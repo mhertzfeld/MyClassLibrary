3c4625eb0c531294c198d574b3347cfbe34b547a
matth <matt_hertzfeld@darice.com>
2013-09-25 12:24:15 -0400

    Added and refactored Mail classes from MyClassLibrary 2008

---
+++ MyClassLibrary/Mail/MailUtilities.cs
@@ -0,0 +1,43 @@
+﻿using MyClassLibrary.Logging;
+using System;
+using System.Diagnostics;
+using System.Net.Mail;
+
+
+namespace MyClassLibrary.Mail
+{
+    public static class MailUtilities
+    {
+        public static Boolean SendMailMessage(MailMessage mailMessage, String smtpHost)
+        {
+            Boolean returnState = true;
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = smtpHost;
+            smtpClient.UseDefaultCredentials = true;
+
+            try
+            {
+                smtpClient.Send(mailMessage);
+            }
+            catch (Exception exception)
+            {
+                returnState = false;
+
+                EnterpriseLibraryLogWriter.WriteExceptionLogEntry(exception);
+            }
+
+            smtpClient = null;
+
+            return returnState;
+        }
+    }
+}
+
+
+
+//MyClassLibrary
+//Copyright (C) 2013 Matthew Hertzfeld https://github.com/mhertzfeld
+//This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+//You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
