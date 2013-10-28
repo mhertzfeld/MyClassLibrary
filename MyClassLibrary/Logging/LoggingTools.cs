86fdaacac9745a26748e068c7a6786e19800c721
matth <matt_hertzfeld@darice.com>
2013-10-28 10:13:12 -0400

    Added new method to LoggingTools

--- MyClassLibrary/Logging/LoggingTools.cs
+++ MyClassLibrary/Logging/LoggingTools.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 
 
 namespace MyClassLibrary.Logging
@@ -10,6 +11,13 @@ namespace MyClassLibrary.Logging
         {
             T_LogWriter logWriter = new T_LogWriter();
             logWriter.WriteToLog(exception);
+        }
+
+        public static void WriteLogEntry<T_LogWriter>(TraceEventType traceEventType, String title, String message, String category = null, Int32 priority = 0, Int32 eventId = 0)
+    where T_LogWriter : Logging.ILogWriter, new()
+        {
+            T_LogWriter logWriter = new T_LogWriter();
+            logWriter.WriteToLog(traceEventType, title, message, category, priority, eventId);
         }
     }
 }
