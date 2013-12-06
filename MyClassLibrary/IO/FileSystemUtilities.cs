ab39af2ef54cd626cdbb5bb9e55b00951e502f69
matth <matt_hertzfeld@darice.com>
2013-12-06 09:45:22 -0500

    Added FileSystemUtilities Class

---
+++ MyClassLibrary/IO/FileSystemUtilities.cs
@@ -0,0 +1,27 @@
+﻿using MyClassLibrary.Logging;
+using System;
+using System.IO;
+
+
+namespace MyClassLibrary.IO
+{
+    public static class FileSystemUtilities
+    {
+        public static Boolean MoveFile<T_LogWriter>(String sourceFileName, String destinationFileName)
+            where T_LogWriter : Logging.ILogWriter, new ()
+        {
+            try
+            {
+                File.Move(sourceFileName, destinationFileName);
+            }
+            catch (Exception exception)
+            {
+                LoggingTools.WriteLogEntry<T_LogWriter>(exception);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
