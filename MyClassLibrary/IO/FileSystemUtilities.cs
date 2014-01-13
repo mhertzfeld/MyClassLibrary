0b67ad4adf2e62aa0574bf0ab9b1c38f48b776f9
matth <matt_hertzfeld@darice.com>
2014-01-13 10:18:10 -0500

    Added a few methods to FileSystemUtilities class

--- MyClassLibrary/IO/FileSystemUtilities.cs
+++ MyClassLibrary/IO/FileSystemUtilities.cs
@@ -7,6 +7,45 @@ namespace MyClassLibrary.IO
 {
     public static class FileSystemUtilities
     {
+        public static Boolean GetDirectoryInfo<T_LogWriter>(String path, out DirectoryInfo directoryInfo)
+            where T_LogWriter : Logging.ILogWriter, new()
+        {
+            try
+            {
+                directoryInfo = new DirectoryInfo(path);
+                //return directoryInfo.Exists;
+            }
+            catch (Exception exception)
+            {
+                LoggingTools.WriteLogEntry<T_LogWriter>(exception);
+
+                directoryInfo = null;
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public static Boolean GetFileInfoArray<T_LogWriter>(DirectoryInfo directoryInfo, out FileInfo[] fileInfoArray)
+            where T_LogWriter : Logging.ILogWriter, new()
+        {
+            try
+            {
+                fileInfoArray = directoryInfo.GetFiles();
+            }
+            catch (Exception exception)
+            {
+                LoggingTools.WriteLogEntry<T_LogWriter>(exception);
+
+                fileInfoArray = null;
+
+                return false;
+            }
+
+            return true;
+        }
+
         public static Boolean MoveFile<T_LogWriter>(String sourceFileName, String destinationFileName)
             where T_LogWriter : Logging.ILogWriter, new ()
         {
