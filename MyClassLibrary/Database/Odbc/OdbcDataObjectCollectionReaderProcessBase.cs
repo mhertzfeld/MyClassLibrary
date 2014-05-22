7093a4d72b9bd50a98d0f2b43d96971180eb31cf
matth <matt_hertzfeld@darice.com>
2014-05-22 15:04:35 -0400

    Renamed file

--- MyClassLibrary/Database/Odbc/OdbcDataObjectCollectionReaderProcessBase.cs
+++ MyClassLibrary/Database/Odbc/OdbcDataObjectCollectionReaderProcessBase.cs
@@ -4,7 +4,7 @@ using System.Data.Odbc;
 
 namespace MyClassLibrary.Database.Odbc
 {
-    public abstract class OdbcDataObjectListReaderProcessBase<T_DataObject, T_DataObjectCollection, T_LogWriter>
+    public abstract class OdbcDataObjectCollectionReaderProcessBase<T_DataObject, T_DataObjectCollection, T_LogWriter>
         : Database.DataObjectCollectionReaderProcessBase<OdbcDatabaseClient<T_LogWriter>, T_DataObject, T_DataObjectCollection, OdbcParameter, OdbcDataReader, OdbcCommand, OdbcConnection, OdbcDataAdapter, OdbcTransaction, T_LogWriter>
         where T_DataObject : new()
         where T_DataObjectCollection : System.Collections.Generic.ICollection<T_DataObject>, new()
