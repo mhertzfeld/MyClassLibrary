602bc727347e8c201480f81712b8d4af92574338
Matthew Hertzfeld <matt.hertzfeld@gmail.com>
2014-07-10 12:09:47 -0400

    Removed old License from .cs files

--- MyClassLibrary/Database/ReaderProcessBase.cs
+++ MyClassLibrary/Database/ReaderProcessBase.cs
@@ -231,11 +231,3 @@ namespace MyClassLibrary.Database
         protected abstract void ProcessRecord(T_DataReader dataReader);
     }
 }
-
-
-
-//MyClassLibrary
-//Copyright (C) 2013 Matthew Hertzfeld https://github.com/mhertzfeld
-//This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-//This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
-//You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
