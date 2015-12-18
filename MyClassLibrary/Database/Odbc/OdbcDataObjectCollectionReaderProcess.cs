80a75cd81231bfabfae54ffed4aa96a75ea770bb
Matthew Hertzfeld <matt.hertzfeld@gmail.com>
2015-12-18 11:06:34 -0500

    Signed-off-by: Matthew Hertzfeld <matt.hertzfeld@gmail.com>

---
+++ MyClassLibrary/Database/Odbc/OdbcDataObjectCollectionReaderProcess.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Odbc;
+
+
+namespace MyClassLibrary.Database.Odbc
+{
+    public class OdbcDataObjectCollectionReaderProcess<T_DataObject, T_DataObjectCollection>
+        : Database.DataObjectCollectionReaderProcess<T_DataObject, OdbcDatabaseClient, T_DataObjectCollection, OdbcParameter, OdbcDataReader, OdbcCommand, OdbcConnection, OdbcDataAdapter, OdbcTransaction>
+        where T_DataObject : DataObjectInterface, new()
+        where T_DataObjectCollection : System.Collections.Generic.ICollection<T_DataObject>, new()
+    {
+
+    }
+}
