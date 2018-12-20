e0723dfba250403561dce28f04541f551f112547
Matthew Hertzfeld <matthew.hertzfeld@protonmail.com>
2018-12-20 15:47:20 -0500

    Signed-off-by: Matthew Hertzfeld <matthew.hertzfeld@protonmail.com>

---
+++ MyClassLibrary_Framework/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MyClassLibrary_Framework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyClassLibrary")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3d2097b3-6006-4cea-9806-d4097694e82e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
---
+++ MyClassLibrary_Standard/Exceptions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyClassLibrary
+{
+    public class PropertySetToDefaultException
+    : System.Exception
+    {
+        public PropertySetToDefaultException(String propertyName)
+            : base("'" + propertyName + "' can not be set to its default.")
+        { }
+    }
+
+    public class PropertySetToOutOfRangeValueException
+    : System.Exception
+    {
+        public PropertySetToOutOfRangeValueException(String propertyName)
+            : base("'" + propertyName + "' can not be set to an out of range vale.")
+        { }
+    }
+
+    public class ValueOutOfRangeException
+    : System.Exception
+    {
+        public ValueOutOfRangeException(String variableName)
+            : base("The value for '" + variableName + "' is out of range.")
+        { }
+    }
+}
